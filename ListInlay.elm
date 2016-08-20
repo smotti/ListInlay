2888d22f7e5d7fdff98df57a9a042c864d030cb3
smotti <wottis@gmail.com>
2016-08-20 19:39:18 +0800

    Create list inlay with buttons

--- ListInlay.elm
+++ ListInlay.elm
@@ -1,9 +1,9 @@
 module ListInlay exposing (main)
 
 import Debug
-import Html exposing (Attribute, Html, a, div, h4, text)
+import Html exposing (Attribute, Html, a, br, button, div, h4, li, span, text, ul)
 import Html.App as App
-import Html.Attributes exposing (attribute, class, href, id, property)
+import Html.Attributes exposing (attribute, class, href, id, property, type')
 import Html.Events exposing (on, onClick, onWithOptions)
 import Json.Decode as Jd
 import Json.Encode as Je
@@ -18,12 +18,21 @@ main =
 
 -- MODEL
 
+type alias Entry =
+  { id : Int
+  , name : String
+  , address : String
+  }
+
 type alias Model =
-  { message : String }
+  { entries : List Entry }
 
 model : Model
 model =
-  { message = "" }
+  { entries = [ {id = 1, name = "Henry Lexington", address = "Nordwalk St."}
+              , {id = 2, name = "Jordan Yellow", address = "Civic Blvd" }
+              ]
+  }
 
 -- UPDATE
 
@@ -34,7 +43,7 @@ type Msg
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Test -> { model | message = "Hello" }
+    Test -> model
 
 
 -- VIEW
@@ -42,60 +51,76 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ div [] [ text <| toString model ]
-    , div
-      [ class "panel-group"
-      , id "accordion"
-      , attribute "role" "tablist"
-      , attribute "aria-multiselectable" "true"
-      ]
-      [ div
-        [ class "panel panel-default" ]
-        [ div
-          [ class "panel-heading"
-          , id "headingOne"
-          , aProperty "role" "tab"
-          , attribute "role" "tab"
-          ]
-          [ h4
-            [ class "panel-title" ]
-            [ a
-              [ attribute "role" "button"
-              , attribute "data-toggle" "collapse"
-              , attribute "data-parent" "#accordion"
-              , href "#collapseOne"
-              , attribute "aria-expanded" "true"
-              , attribute "aria-controls" "collapseOne"
-              , onClick Test
-              ]
-              [ text "Collapsible Group Item #1" ]
-            ] -- collapse btn one
-          ] -- head collapse grp one
-        , div
-          [ id "collapseOne"
-          , class "panel-collapse collapse"
-          , attribute "role" "tabpanel"
-          , attribute "aria-labelledby" "headingOne"
-          ]
-          [ div [ class "panel-body" ]
-            [ text model.message ]
-          ] -- grp one body
-        ] -- panel collapse grp one
-      ] -- panel grp
+    [ (viewEntries model.entries) ]
+
+
+viewEntries : List Entry -> Html Msg
+viewEntries es =
+  div
+    [ class "panel-group container-fluid"
+    , id "accordion"
+    , attribute "role" "tablist"
+    , attribute "aria-multiselectable" "true"
     ]
+    (List.map viewEntry es)
 
 
-aProperty : String -> String -> Attribute msg
-aProperty name value =
-  property name (Je.string value)
-
-
-{-- They ain't working :/.
-showInlay : Msg -> Attribute Msg
-showInlay msg =
-  on "shown.bs.collapse" (Jd.succeed msg)
-
-showCollapse : Msg -> Attribute Msg
-showCollapse msg =
-  onWithOptions "show.bs.collapse" { stopPropagation = True, preventDefault = True } (Jd.succeed msg)
---}
+viewEntry : Entry -> Html Msg
+viewEntry e =
+  div
+    [ class "panel panel-default row" ]
+    [ div
+      [ class "panel-heading col-sm-12"
+      , id <| "entry-" ++ (toString e.id)
+      , attribute "role" "tab"
+      ]
+      [ div [ class "panel-title" ]
+        [ div [ class "row", id <| "entryValues-" ++ (toString e.id) ]
+          [ div [ class "col-xs-12 col-sm-6" ]
+            [ text "Name:"
+            , br [] []
+            , text e.name
+            ]
+          , div [ class "col-xs-12 col-sm-6" ]
+            [ text "Address:"
+            , br [] []
+            , text e.address
+            ]
+          ]
+        , div [ class "row", id <| "entryOps-" ++ (toString e.id) ]
+          [ div [ class "col-xs-12 col-sm-6" ]
+            [ text <| "Some information about entry-" ++ (toString e.id) ]
+          , div [ class "col-xs-12 col-sm-6" ]
+            [ span
+              []
+              [ button [ class "btn-xs btn-info" ]
+                [ span [ class "glyphicon glyphicon-pencil" ] [] ]
+              , button
+                [ class "btn-xs btn-info"
+                , attribute "role" "button"
+                , attribute "data-toggle" "collapse"
+                , attribute "data-parent" "#accordion"
+                , attribute "data-target" <| "#entryInfo-" ++ (toString e.id)
+                , attribute "aria-expanded" "false"
+                , attribute "aria-controls" <| "entryInfo-" ++ (toString e.id)
+                , type' "button"
+                ]
+                [ span [ class "glyphicon glyphicon-info-sign" ] [] ]
+              , button [ class "btn-xs btn-danger" ]
+                [ span [ class "glyphicon glyphicon-remove" ] [] ]
+              ]
+            ]
+          ]
+          , div
+            [ class "row panel-collapse collapse"
+            , id <| "entryInfo-" ++ (toString e.id)
+            , attribute "role" "tabpanel"
+            , attribute "aria-labelledby" <| "entryValues-" ++ (toString e.id)
+            ]
+            [ div
+              [ class "panel-body col-xs-12 col-sm-12" ]
+              [ text "Entry Detail" ]
+            ]
+          ] 
+        ]
+      ]
